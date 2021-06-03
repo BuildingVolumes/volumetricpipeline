638e134b26d2ff5580c7963205bf93f0bf18c21a
syberspaz <syberspaz@gmail.com>
2021-06-03 19:05:02 -0400

    pymeshlabscripts
    added a python script for pymeshlab to batch ply files, currently can just fix the ply from LIvescan so Unity can load it properly in PCX, also can run poisson recon

---
+++ Scripts/pyMeshlabScripts/batch_export_ply.py
@@ -0,0 +1,64 @@
+import pymeshlab
+import os
+import sys, getopt
+
+
+
+def main(argv):
+   inputDir = ''
+   outputDir = ''
+   oSpecified = False
+   recon = False
+   try:
+      opts, args = getopt.getopt(argv,"hd:o:r",["dir=","out="])
+   except getopt.GetoptError:
+      print('test.py -d <data dir> -o <optional output dir> -r')
+      sys.exit(2)
+   if( len(opts) == 0):
+      	print('test.py -d <data dir> -o <optional output dir> -r (reconstruct)')
+      	sys.exit(2)
+   for opt, arg in opts:
+      if opt == '-h':
+         print('test.py -d <data dir> -o <optional output dir> -r (reconstruct)')
+         sys.exit()
+      elif opt in ("-d", "--dir"):
+         inputDir = arg
+      elif opt in ("-o", "--out"):
+        outputDir = arg
+        oSpecified = True
+      elif opt in ("-r","--recon"):
+         recon = True
+
+   if(oSpecified == False):
+      outputDir = inputDir
+
+   print('Input DIR is ' +inputDir)
+   print('Output DIR is '+ outputDir)
+   for filename in os.listdir(inputDir):
+      if filename.endswith(".ply") :
+         fname = os.path.join(inputDir, filename)
+         ofname = os.path.join(outputDir,filename)
+         print('processing:'+fname + " --> "+ofname)
+         ms = pymeshlab.MeshSet()
+         ms.load_new_mesh(fname)
+            
+         if recon == True :
+            print("recon:")  
+            ms.compute_normals_for_point_sets()
+            ms.per_vertex_normal_function()
+            ms.surface_reconstruction_screened_poisson()
+ 
+         ms.save_current_mesh(ofname)
+      else:
+         continue
+
+
+
+
+
+
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
+
+
